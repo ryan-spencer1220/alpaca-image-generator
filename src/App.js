2c88c63b2488f3090bbbaf4d7705e79a8ea654b3
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-08-19 14:06:59 -0700

    add utils file attribute arrays

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import { useState } from "react";
-import BlueBackground from "../src/alpaca/backgrounds/blue50.png";
+import BlueBackground from "../src/alpaca/backgrounds/darkblue70.png";
+import DefaultEars from "../src/alpaca/ears/default.png";
+import DefaultEyes from "../src/alpaca/eyes/default.png";
+import DefaultHair from "../src/alpaca/hair/default.png";
+import DefaultLeg from "../src/alpaca/leg/default.png";
+import DefaultMouth from "../src/alpaca/mouth/default.png";
+import DefaultNeck from "../src/alpaca/neck/default.png";
+import Nose from "../src/alpaca/nose.png";
 
 function App() {
   const [background, setBackground] = useState(BlueBackground);
-  const [ears, setEars] = useState();
-  const [eyes, setEyes] = useState();
-  const [hair, setHair] = useState();
-  const [leg, setLeg] = useState();
-  const [mouth, setMouth] = useState();
-  const [neck, setNeck] = useState();
-  const [accessories, setAccessories] = useState();
+  const [ears, setEars] = useState(DefaultEars);
+  const [eyes, setEyes] = useState(DefaultEyes);
+  const [hair, setHair] = useState(DefaultHair);
+  const [leg, setLeg] = useState(DefaultLeg);
+  const [mouth, setMouth] = useState(DefaultMouth);
+  const [neck, setNeck] = useState(DefaultNeck);
+  const [accessories, setAccessories] = useState(null);
+  const [selectedAttribute, setSelectedAttribute] = useState();
 
   return (
-    <div className="card flex flex-wrap items-center h-72 w-full bg-gray-100 shadow-2xl m-36">
-      <div className="card-body">
-        <div className="card-title text-5xl">Alpaca Generator</div>
-        <img src={background} alt="" height="300px" width="300px" />
+    <div className="card grid grid-cols-2 items-center bg-gray-100 shadow-2xl m-52 space-x-4 h-max">
+      <div className="card-body m-8">
+        <div className="card-title text-5xl uppercase my-4">
+          alpaca generator
+        </div>
+        <div className="relative">
+          <img
+            src={background}
+            alt=""
+            height="400px"
+            width="400px"
+            className="z-0"
+          />
+          <img
+            src={ears}
+            alt=""
+            height="400px"
+            width="400px"
+            className="absolute z-10"
+          />
+        </div>
+        <div className="m-4 space-x-4">
+          <button className="btn btn-primary">Random</button>
+          <button className="btn btn-primary">Download</button>
+        </div>
       </div>
-      <div className="card-body">
-        <div className="card-title text-5xl">Alpaca Generator</div>
-        <img src={background} alt="" height="300px" width="300px" />
+      <div className="card-body m-10">
+        <div className="grid grid-cols-1">
+          <div className="my-10 ">
+            <h6 className="text-2xl font-bold uppercase my-4">
+              Accessorize The Alapacas
+            </h6>
+            <div className="grid grid-cols-3">
+              <button className="btn btn-outline btn-primary px-20 m-1">
+                Hair
+              </button>
+              <button className="btn btn-outline btn-primary m-1 active:btn-active">
+                Ears
+              </button>
+              <button className="btn btn-outline btn-primary m-1">Eyes</button>
+              <button className="btn btn-outline btn-primary m-1">Mouth</button>
+              <button className="btn btn-outline btn-primary m-1">Neck</button>
+              <button className="btn btn-outline btn-primary m-1">Leg</button>
+              <button className="btn btn-outline btn-primary m-1">
+                Accessories
+              </button>
+              <button className="btn btn-outline btn-primary m-1">
+                Background
+              </button>
+            </div>
+          </div>
+          <h6 className="text-2xl font-bold uppercase">Style</h6>
+        </div>
       </div>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const accessories = ["Earrings", "Flower", "Glasses", "Headphone"];
+const backgrounds = ["Blue", "DarkBlue", "Green", "Grey", "Red", "Yellow"];
+const ears = ["Default", "Backward", "Forward"];
+const eyes = ["Angry", "Default", "Naughty", "Panda", "Smart", "Star"];
+const hair = ["Bang", "Curls", "Default", "Elegant", "Fancy", "Quiff", "Short"];
+const leg = [
+  "BubbleTea",
+  "Cookie",
+  "Default",
+  "GameConsole",
+  "Backward",
+  "Forward",
+];
+const mouth = ["Astonished", "Default", "Eating", "Laugh", "Tongue"];
+const neck = ["Backward", "Forward", "Default", "Thick"];
