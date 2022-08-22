223c30cc84eb25074b9c86a6f28ac9f7733d1a4a
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-08-22 13:59:31 -0700

    reformat for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   accessoryNames,
   backgroundNames,
@@ -9,42 +9,68 @@ import {
   neckNames,
   legNames,
 } from "./utils";
-import BlueBackground from "../src/alpaca/backgrounds/darkblue70.png";
-import DefaultEars from "../src/alpaca/ears/default.png";
-import DefaultEyes from "../src/alpaca/eyes/default.png";
-import DefaultHair from "../src/alpaca/hair/default.png";
-import DefaultLeg from "../src/alpaca/leg/default.png";
-import DefaultMouth from "../src/alpaca/mouth/default.png";
-import DefaultNeck from "../src/alpaca/neck/default.png";
-import Headphones from "../src/alpaca/accessories/headphone.png";
 import Nose from "../src/alpaca/nose.png";
 
 function App() {
-  const [background, setBackground] = useState(BlueBackground);
-  const [ears, setEars] = useState(DefaultEars);
-  const [eyes, setEyes] = useState(DefaultEyes);
-  const [hair, setHair] = useState(DefaultHair);
-  const [leg, setLeg] = useState(DefaultLeg);
-  const [mouth, setMouth] = useState(DefaultMouth);
-  const [neck, setNeck] = useState(DefaultNeck);
+  const [background, setBackground] = useState(backgroundNames.DarkBlue);
+  const [ears, setEars] = useState(earNames.Default);
+  const [eyes, setEyes] = useState(eyeNames.Default);
+  const [hair, setHair] = useState(hairNames.Default);
+  const [leg, setLeg] = useState(legNames.Default);
+  const [mouth, setMouth] = useState(mouthNames.Default);
+  const [neck, setNeck] = useState(neckNames.Default);
   const [accessories, setAccessories] = useState(accessoryNames.Headphone);
   const [selectedAttribute, setSelectedAttribute] = useState(hairNames);
+  const [currentAccessory, setCurrentAccessory] = useState("hair");
 
-  const changeStyles = (e) => {
-    e.preventDefault();
-    const array = e.target.value.split(",");
-    setSelectedAttribute(array);
-  };
+  useEffect(() => {
+    console.log(
+      "Ears: ",
+      ears,
+      "Eyes: ",
+      eyes,
+      "Hair: ",
+      hair,
+      "Leg: ",
+      leg,
+      "Mouth: ",
+      mouth,
+      "Neck: ",
+      neck,
+      "Accessories: ",
+      accessories,
+      "Background: ",
+      background
+    );
+  }, [background, ears, eyes, hair, leg, mouth, neck, accessories]);
 
-  const changeAccessory = (value) => {
-    console.log(value);
-    console.log("HIT");
-    setAccessories(value);
+  const changeAccessory = (key, value) => {
+    console.log(currentAccessory);
+    switch (currentAccessory) {
+      case "background":
+        setBackground(value);
+      case "ears":
+        setEars(value);
+      case "eyes":
+        setEyes(value);
+      case "hair":
+        setHair(value);
+      case "leg":
+        setLeg(value);
+      case "mouth":
+        setMouth(value);
+      case "neck":
+        setNeck(value);
+      case "accessories":
+        setAccessories(value);
+      default:
+        setBackground(backgroundNames.DarkBlue);
+    }
   };
 
   return (
-    <div className="card grid grid-cols-2 items-center bg-gray-100 shadow-2xl m-52 space-x-4 h-max">
-      <div className="card-body m-8">
+    <div className="card grid grid-cols-2 items-center bg-gray-100 shadow-2xl m-52">
+      <div className="card-body mb-96">
         <div className="card-title text-5xl uppercase my-4">
           alpaca generator
         </div>
@@ -54,42 +80,42 @@ function App() {
             alt=""
             height="400px"
             width="400px"
-            className="pt-8"
+            className="absolute"
           />
           <img
             src={ears}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={neck}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={hair}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={accessories}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={eyes}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
 
           <img
@@ -97,21 +123,21 @@ function App() {
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={mouth}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
           <img
             src={leg}
             alt=""
             height="400px"
             width="400px"
-            className="absolute -mt-96"
+            className="absolute"
           />
         </div>
         <div className="m-4 space-x-4">
@@ -119,84 +145,102 @@ function App() {
           <button className="btn btn-primary">Download</button>
         </div>
       </div>
-      <div className="card-body m-10">
+      <div className="card-body">
         <div className="grid grid-cols-1">
-          <div className="my-10 ">
+          <div className="my-10">
             <h6 className="text-2xl font-bold uppercase my-4">
               Accessorize The Alapacas
             </h6>
             <div className="flex flex-wrap">
               <button
                 className="btn btn-outline btn-primary m-1 flex-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(hairNames);
+                  setCurrentAccessory("hair");
+                }}
                 value={hairNames}
               >
                 Hair
               </button>
               <button
                 className="btn btn-outline btn-primary m-1 active:btn-active flex-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(earNames);
+                  setCurrentAccessory("ears");
+                }}
                 value={earNames}
               >
                 Ears
               </button>
               <button
                 className="btn btn-outline btn-primary m-1 flex-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(eyeNames);
+                  setCurrentAccessory("eyes");
+                }}
                 value={eyeNames}
               >
                 Eyes
               </button>
               <button
                 className="btn btn-outline btn-primary m-1 flex-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(mouthNames);
+                  setCurrentAccessory("mouth");
+                }}
                 value={mouthNames}
               >
                 Mouth
               </button>
               <button
                 className="btn btn-outline btn-primary m-1 flex-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(neckNames);
+                  setCurrentAccessory("neck");
+                }}
                 value={neckNames}
               >
                 Neck
               </button>
               <button
                 className="btn btn-outline btn-primary m-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(legNames);
+                  setCurrentAccessory("leg");
+                }}
                 value={legNames}
               >
                 Leg
               </button>
               <button
                 className="btn btn-outline btn-primary m-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(accessoryNames);
+                  setCurrentAccessory("hair");
+                }}
                 value={accessoryNames}
               >
                 Accessories
               </button>
               <button
                 className="btn btn-outline btn-primary m-1"
-                onClick={changeStyles}
+                onClick={() => {
+                  setSelectedAttribute(backgroundNames);
+                  setCurrentAccessory("background");
+                }}
                 value={backgroundNames}
               >
                 Background
               </button>
             </div>
           </div>
-          <h6
-            className="text-2xl font-bold uppercase"
-            onClick={changeStyles}
-            value={earNames}
-          >
-            Style
-          </h6>
+          <h6 className="text-2xl font-bold uppercase">Style</h6>
           <div className="grid grid-cols-3">
-            {Object.entries(accessoryNames).map(([key, value]) => (
+            {Object.entries(selectedAttribute).map(([key, value]) => (
               <button
                 className="btn btn-outline btn-primary m-1"
                 key={key}
-                onClick={() => changeAccessory(value)}
+                onClick={() => changeAccessory(key, value)}
               >
                 {key}
               </button>
--- src/utils.js
+++ src/utils.js
@@ -13,37 +13,48 @@ export const backgroundNames = {
   Red: require("../src/alpaca/backgrounds/red70.png"),
   Yellow: require("../src/alpaca/backgrounds/yellow70.png"),
 };
-export const earNames = ["Default", "Backward", "Forward"];
-export const eyeNames = [
-  "Angry",
-  "Default",
-  "Naughty",
-  "Panda",
-  "Smart",
-  "Star",
-];
-export const hairNames = [
-  "Bang",
-  "Curls",
-  "Default",
-  "Elegant",
-  "Fancy",
-  "Quiff",
-  "Short",
-];
-export const legNames = [
-  "BubbleTea",
-  "Cookie",
-  "Default",
-  "GameConsole",
-  "Backward",
-  "Forward",
-];
-export const mouthNames = [
-  "Astonished",
-  "Default",
-  "Eating",
-  "Laugh",
-  "Tongue",
-];
-export const neckNames = ["Backward", "Forward", "Default", "Thick"];
+
+export const earNames = {
+  Default: require("../src/alpaca/ears/default.png"),
+  Backward: require("../src/alpaca/ears/tilt-backward.png"),
+  Forward: require("../src/alpaca/ears/tilt-forward.png"),
+};
+
+export const eyeNames = {
+  Angry: require("../src/alpaca/eyes/angry.png"),
+  Default: require("../src/alpaca/eyes/default.png"),
+  Naughty: require("../src/alpaca/eyes/naughty.png"),
+  Panda: require("../src/alpaca/eyes/panda.png"),
+  Smart: require("../src/alpaca/eyes/smart.png"),
+  Star: require("../src/alpaca/eyes/star.png"),
+};
+export const hairNames = {
+  Bang: require("../src/alpaca/hair/bang.png"),
+  Curls: require("../src/alpaca/hair/curls.png"),
+  Default: require("../src/alpaca/hair/default.png"),
+  Elegant: require("../src/alpaca/hair/elegant.png"),
+  Fancy: require("../src/alpaca/hair/fancy.png"),
+  Quiff: require("../src/alpaca/hair/quiff.png"),
+  Short: require("../src/alpaca/hair/short.png"),
+};
+export const legNames = {
+  BubbleTea: require("../src/alpaca/leg/bubble-tea.png"),
+  Cookie: require("../src/alpaca/leg/cookie.png"),
+  Default: require("../src/alpaca/leg/default.png"),
+  GameConsole: require("../src/alpaca/leg/game-console.png"),
+  Backward: require("../src/alpaca/leg/tilt-backward.png"),
+  Forward: require("../src/alpaca/leg/tilt-forward.png"),
+};
+export const mouthNames = {
+  Astonished: require("../src/alpaca/mouth/astonished.png"),
+  Default: require("../src/alpaca/mouth/default.png"),
+  Eating: require("../src/alpaca/mouth/eating.png"),
+  Laugh: require("../src/alpaca/mouth/laugh.png"),
+  Tongue: require("../src/alpaca/mouth/tongue.png"),
+};
+export const neckNames = {
+  Backward: require("../src/alpaca/neck/bend-backward.png"),
+  Forward: require("../src/alpaca/neck/bend-forward.png"),
+  Default: require("../src/alpaca/neck/default.png"),
+  Thick: require("../src/alpaca/neck/thick.png"),
+};
